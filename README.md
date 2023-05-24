5b466b8eea9822f8a111a34b9919464b281391e0
tom <blessontomjoseph@gmail.com>
2023-05-25 04:45:25 +0530

    all files

---
+++ README.md
@@ -0,0 +1,19 @@
+# Fine-tuning a Text-to-Speech Model with Emotion Labels
+
+This repository provides a guide on how to fine-tune a text-to-speech (TTS) model using a dataset that includes emotion labels for the speech. Fine-tuning a TTS model with emotion labels can enable the generation of emotionally expressive speech.
+
+## Dataset
+
+The [IEMOCAP dataset](https://www.kaggle.com/datasets/mouadriali/iemocap-transcriptions-english-french) is used for fine-tuning, consists of speech samples paired with corresponding emotion labels. Each sample in the dataset includes a text prompt and the corresponding audio waveform, along with an emotion label indicating the intended emotional expression of the speech.
+
+## Model
+
+The base TTS model used for fine-tuning is [microsoft/speecht5_tts](https://huggingface.co/microsoft/speecht5_tts), a state-of-the-art text-to-speech model.
+
+## Webapp
+[Try generating speech]()
+
+## Links
+The [text_to_speech notebook](text-to-speech.ipynb) was trained on kaggle, for reproducing the results or for running, fork my notebook 👉 [notebook](https://www.kaggle.com/code/blessontomjoseph/text-to-speech/edit)
+
+Find the fine tuned model on huggingface hub 👉 [model](https://huggingface.co/theothertom/emo_t5_speech_chkpt/tree/main)
---
+++ app.py
@@ -0,0 +1,34 @@
+from transformers import AutoProcessor, SpeechT5ForTextToSpeech
+import torch
+from transformers import SpeechT5HifiGan
+from IPython.display import Audio
+import streamlit as st
+import time
+
+def gen(prompt,emo,model,speaker_embeddings):
+    text=prompt+" [emotion] "+emo
+    inputs = processor(text=text, return_tensors="pt")
+    spectrogram = model.generate_speech(inputs["input_ids"].to('cpu'), speaker_embeddings.to('cpu'))
+    with torch.no_grad():
+            speech=vocoder(spectrogram)
+    return Audio(speech.cpu().numpy(),rate=16000,autoplay=True)
+
+
+if __name__ == '__main__':
+    text = st.text_input("Enter your text")
+    emo = st.sidebar.radio("Select an emo!", ('Frustration','Excitement','Neutral','Anger','Sadness','Happiness','Surprise','Fear','Other','Disgust'))
+    apply = st.button('generate speech')
+    if apply:
+        with st.spinner("getting model from 🤗..."):
+            processor = AutoProcessor.from_pretrained("theothertom/emo_t5_speech_chkpt")
+            model = SpeechT5ForTextToSpeech.from_pretrained("theothertom/emo_t5_speech_chkpt")
+            vocoder = SpeechT5HifiGan.from_pretrained("microsoft/speecht5_hifigan") 
+        st.success('model ready!')
+
+        with st.spinner("generating caption..."):
+            start_time = time.time()
+            speech=gen(text,emo,model.to('cpu'),torch.load('speaker_embeddings.pt'))
+            response_time = time.time() - start_time
+
+        st.success(f"""Success!~Elapsed Time: {response_time:.2f} seconds""")
+        st.write(speech)
